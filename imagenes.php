759d998ad84ef30d0936a43c5536529e8b6c574d
default <oskar.sajek@educa.madrid.org>
2025-11-28 09:37:51 +0000

    imagenes bien

--- imagenes.php
+++ imagenes.php
@@ -1,7 +1,7 @@
 <?php
 
-for ( $i = 1973; $i <= 2025; $i +1 ){
-    $imagen = "/home/coderuser/public-html/imagenes" . $i . ".png";
-    echo "<img src=" . $imagen . "<br>";
+for ($i = 1973; $i <= 2025; $i = $i + 1){
+    $imagen = "https://www.glerl.noaa.gov/data/ice/max_anim/png/" . $i . ".png";
+    echo ("<img src=" . $imagen . ">");
 }
 ?>
