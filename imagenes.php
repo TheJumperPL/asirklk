ef8c96c454ddfcb806c4ef8595819882e983cd68
default <oskar.sajek@educa.madrid.org>
2025-11-28 09:31:19 +0000

    las imagenes del hielo

---
+++ imagenes.php
@@ -0,0 +1,7 @@
+<?php
+
+for ( $i = 1973; $i <= 2025; $i +1 ){
+    $imagen = "/home/coderuser/public-html/imagenes" . $i . ".png";
+    echo "<img src=" . $imagen . "<br>";
+}
+?>
---
+++ prueba.php
@@ -0,0 +1,7 @@
+<?php
+echo "alibaba";
+
+$variable = 9;
+
+echo ($variable*3);
+?>
